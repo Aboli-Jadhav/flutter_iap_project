c489e8c815710c888541aa636e325df791c46556
Dhairysheel-Deshmukh <dhairysheel06@gmail.com>
2021-11-01 20:49:39 +0530

    commit !!!9

--- lib/master_admin/view_gauge/view_Master_guage.dart
+++ lib/master_admin/view_gauge/view_Master_guage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'gauge_desc.dart';
+import 'package:editable/editable.dart';
 
 class view_master_gau extends StatefulWidget {
   const view_master_gau({Key? key}) : super(key: key);
@@ -15,6 +15,32 @@ class _view_master_gauState extends State<view_master_gau> {
   Color backred=Color(0xffDF3F3F);
   Color lred=Color(0xffFBEBEB);
 
+  //row data
+  List rows = [
+  {"id": 'WP36473647364', "type":'Type A',"size":'3',"date":'01/01/2021',"location":'Pune'},
+    {"id": 'WP36473647364', "type":'Type A',"size":'3',"date":'01/01/2021',"location":'Pune'},
+    {"id": 'WP36473647364', "type":'Type A',"size":'3',"date":'01/01/2021',"location":'Pune'},
+    {"id": 'WP36473647364', "type":'Type A',"size":'3',"date":'01/01/2021',"location":'Pune'},
+    {"id": 'WP36473647364', "type":'Type A',"size":'3',"date":'01/01/2021',"location":'Pune'},
+    {"id": 'WP36473647364', "type":'Type A',"size":'3',"date":'01/01/2021',"location":'Pune'},
+    {"id": 'WP36473647364', "type":'Type A',"size":'3',"date":'01/01/2021',"location":'Pune'},
+    {"id": 'WP36473647364', "type":'Type A',"size":'3',"date":'01/01/2021',"location":'Pune'},
+    {"id": 'WP36473647364', "type":'Type A',"size":'3',"date":'01/01/2021',"location":'Pune'},
+    {"id": 'WP36473647364', "type":'Type A',"size":'3',"date":'01/01/2021',"location":'Pune'},
+    {"id": 'WP36473647364', "type":'Type A',"size":'3',"date":'01/01/2021',"location":'Pune'},
+    {"id": 'WP36473647364', "type":'Type A',"size":'3',"date":'01/01/2021',"location":'Pune'},
+    {"id": 'WP36473647364', "type":'Type A',"size":'3',"date":'01/01/2021',"location":'Pune'},
+    {"id": 'WP36473647364', "type":'Type A',"size":'3',"date":'01/01/2021',"location":'Pune'},
+  ];
+
+  List headers = [
+  {"title":'Gauge ID No.', 'index': 1, 'key':'id'},
+  {"title":'Gauge Type', 'index': 2, 'key':'type'},
+  {"title":'Gauge Size', 'index': 3, 'key':'size'},
+  {"title":'Due Date', 'index': 4, 'key':'date'},
+  {"title":'Location', 'index': 5, 'key':'location'},
+  ];
+
   @override
   Widget build(BuildContext context) {
 
@@ -22,38 +48,23 @@ class _view_master_gauState extends State<view_master_gau> {
       child: Scaffold(
 
 
-          body:ListView(
-            padding: const EdgeInsets.all(40),
-            children: <Widget>[
-              Column(
-                children: [
-                  Row(
-                    children:[
-                      gauge_desc(),
-                      SizedBox(width: 15,),
-                      gauge_desc(),
-                      SizedBox(width: 15,),
-                      gauge_desc(),
-                      SizedBox(width: 15,),
-                      gauge_desc(),
-                    ]
-                  ),
-                  SizedBox(height: 25,),
-                  Row(
-                    children:[
-                      gauge_desc(),
-                      SizedBox(width: 15,),
-                      gauge_desc(),
-                      SizedBox(width: 15,),
-                      gauge_desc(),
-                      SizedBox(width: 15,),
-                      gauge_desc(),
-                    ]
-                  )
-                ],
-              )
-            ],
-          )
+          body:Editable(
+            columns: headers,
+            rows: rows,
+            showCreateButton: false,
+            tdStyle: TextStyle(fontSize: 20),
+            showSaveIcon: false,
+            tdAlignment: TextAlign.center,
+            thAlignment: TextAlign.center,
+            thPaddingBottom: 12,
+            thPaddingTop: 12,
+            trHeight: 40,
+            columnRatio: 0.196,
+            borderColor: Colors.blue.shade900,
+            focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.blue),
+                borderRadius: BorderRadius.all(Radius.circular(0))),
+          ),
       ),
     );
   }
