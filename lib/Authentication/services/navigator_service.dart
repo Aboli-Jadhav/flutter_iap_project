aaae8e673071ae45708268f7cb9ad09d0d967d1e
Dhairysheel-Deshmukh <dhairysheel06@gmail.com>
2022-01-05 19:10:27 +0530

    pop screen

--- lib/Authentication/services/navigator_service.dart
+++ lib/Authentication/services/navigator_service.dart
@@ -11,11 +11,11 @@ class NavigationService {
 
   Future<dynamic> navigateTo(int r, {dynamic arguments}) {
     if(r==2)
-      return _navigationKey.currentState!.pushNamed('/adminpage', arguments: arguments);
+      return _navigationKey.currentState!.popAndPushNamed('/adminpage', arguments: arguments);
     else if(r==1)
-      return _navigationKey.currentState!.pushNamed('/master_adminHome', arguments: arguments);
+      return _navigationKey.currentState!.popAndPushNamed('/master_adminHome', arguments: arguments);
     else
-      return _navigationKey.currentState!.pushNamed('/viewerhome', arguments: arguments);
+      return _navigationKey.currentState!.popAndPushNamed('/viewerhome', arguments: arguments);
 
   }
 }
